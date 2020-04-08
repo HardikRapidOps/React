b798f0b1eca1ea426a24e020174ba9a8f3de034f
Hardik Motwani <motwanihardik@gmail.com>
2020-04-08 15:10:11 +0530

    React Application

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
+import Message from './components/Message';
+import Counter from './components/Counter';
+import Watch from './components/Watch';
+function App() {
+  return (
+    <div className="App">
+      <Watch />
+    </div>
+  );
+}
+
+export default App;
